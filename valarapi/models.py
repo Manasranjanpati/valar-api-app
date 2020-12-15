f7421f8f2bc26a24818d469aa8a9da74006d2c39
Manas Ranjan Pati <iammanas24@gmail.com>
2020-12-15 11:16:24 +0530

    Quote model with user as Author

--- valarapi/models.py
+++ valarapi/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Quote(models.Model):
+    text = models.TextField()
+    author = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        blank=True, null=True
+    )
+
+    def __str__(self):
+        return f"{self.text} by {self.author} (submitted by {self.user})"
+
+    def get_absolute_url(self):
+        return reverse('quote_detail', args=f"{self.id}")
+
+    class Meta:
+        ordering = ("-created",)
